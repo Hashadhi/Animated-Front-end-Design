9dc71c47ac0ad8ab1d47dd5f1dffc3f0bae6b5bc
Hashadhi <hashadhijayasinghe@gmail.com>
2022-04-16 14:18:56 +0530

    Add stage and animate text-cursor

--- js/mouse-pointer/app.js
+++ js/mouse-pointer/app.js
@@ -6,26 +6,26 @@ elmCircle.classList.add('pointer');
 elmCircle.classList.add('hide');
 document.body.append(elmCircle);
 
-addEventListener('mousemove', function(e){
+// addEventListener('mousemove', function(e){
 
-    if(tmrId){
-        clearTimeout(tmrId);
-        tmrId = null;
-    }
+//     if(tmrId){
+//         clearTimeout(tmrId);
+//         tmrId = null;
+//     }
 
-    tmrId = this.setTimeout(function(){
-        elmCircle.classList.add('hide');
-    }, 2000);
+//     tmrId = this.setTimeout(function(){
+//         elmCircle.classList.add('hide');
+//     }, 2000);
 
-    if(elmCircle.classList.contains('hide')){
-        elmCircle.classList.remove('hide');
-    }
+//     if(elmCircle.classList.contains('hide')){
+//         elmCircle.classList.remove('hide');
+//     }
 
-    var xPosition = e.pageX;
-    var yPosition = e.pageY;
+//     var xPosition = e.pageX;
+//     var yPosition = e.pageY;
 
-    elmCircle.style.left = xPosition + 'px';
-    elmCircle.style.top = yPosition + 'px';
+//     elmCircle.style.left = xPosition + 'px';
+//     elmCircle.style.top = yPosition + 'px';
 
 
-});
+// });
